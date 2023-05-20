f7be6e6c8fa005130b4fb4a2b02991654dcad529
Alessandra Wong <74124296+awong-pdx@users.noreply.github.com>
2023-05-20 11:37:12 -0700

    Implement theme provider (#21)
    * Added Theme custom context provider and toggle btn
    
    * Added react-bootstrap button to toggle
    
    * Imported react-bootstrap-icons for button images
    
    * Clean up comments and variable names
    
    * Adjusted bootstrap classNames for responsiveness
    
    * Added `section` elements, replaced App className

---
+++ src/components/ToggleButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { MoonFill, SunFill } from 'react-bootstrap-icons';
+import { useTheme } from './ThemeProvider';
+
+export default function ToggleButton() {
+  const { theme, toggleTheme } = useTheme();
+  const iconToggleDarkOn = <MoonFill size={32} />;
+  const iconToggleDarkOff = <SunFill size={32} />;
+  const btnIcon = theme === 'dark' ? iconToggleDarkOn : iconToggleDarkOff;
+  const btnText = theme === 'dark' ? `  Dark` : `  Light`;
+
+  const handleClick = () => {
+    toggleTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <button
+      type="button"
+      className={`btn btn-${theme}`}
+      onClick={() => {
+        handleClick();
+      }}
+    >
+      {btnIcon}
+      {btnText}
+    </button>
+  );
+}
