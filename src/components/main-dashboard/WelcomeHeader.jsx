58d8daf82f0ad08c3b5ed4ab5e13e7f532a47962
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-05-27 14:00:20 -0700

    Add time functionality

--- src/components/main-dashboard/WelcomeHeader.jsx
+++ src/components/main-dashboard/WelcomeHeader.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrentTime } from '../../utilities/customHooks';
 
 export default function WelcomeHeader() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(getCurrentTime());
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
-    <div className="row mb-5">
-      <h1>Welcome to our weather dashboard!</h1>
+    <div className="row justify-content-end mb-2">
+      <div className="col-6 text-end">
+        <p>{time}</p>
+        <h2>Welcome to our weather dashboard!</h2>
+      </div>
     </div>
   );
 }
