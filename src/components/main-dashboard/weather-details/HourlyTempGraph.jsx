a36a0de9593033034390db05d1f2a5868d61e203
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-01 15:19:19 -0700

    Get hours for x-values line graph

--- src/components/main-dashboard/weather-details/HourlyTempGraph.jsx
+++ src/components/main-dashboard/weather-details/HourlyTempGraph.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useWeather } from '../../WeatherProvider';
+import { toLocalDate, getHourString } from '../../../utilities/helperFunctions';
+// import { Line } from 'react-chartjs-2';
 
 export default function HourlyTempGraph() {
+  const { hourlyWeather } = useWeather();
+  if (!hourlyWeather) return null;
+
+  const { timezone } = hourlyWeather.city;
+  const hourlyTimeXValues = hourlyWeather.list
+    .slice(0, 5)
+    .map((weatherForHour) =>
+      getHourString(toLocalDate(weatherForHour.dt), timezone)
+    );
+
+  console.log(hourlyTimeXValues);
+
   return (
     <div className="col-8 border border-primary rounded">
       <h1>Hourly Weather Graph</h1>
