4f4ad82587d8074de949ddb0a4cfaf8b9c71ab72
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-02 12:05:04 -0700

    Display humidity levels and graph

--- src/components/main-dashboard/weather-details/Humidity.jsx
+++ src/components/main-dashboard/weather-details/Humidity.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import ProgressBar from '@ramonak/react-progress-bar';
 import { useWeather } from '../../WeatherProvider';
 import humidityIcon from '../../../images/humidityIcon.png';
 
 export default function Humidity() {
   const { currentWeather } = useWeather();
   if (!currentWeather) return null;
+  let humidityDescription = 'Ideal humidity levels';
 
   const { humidity } = currentWeather.main;
-
-  // DELETE LATER
-  console.log(humidity);
+  if (humidity >= 70 || humidity < 25) {
+    humidityDescription = 'Poor humidity levels';
+  } else if (
+    (humidity < 70 && humidity >= 60) ||
+    (humidity >= 25 && humidity < 30)
+  ) {
+    humidityDescription = 'Fair humidity levels';
+  }
 
   return (
-    <div className="col-4 border border-primary rounded">
+    <div className="col-4 border border-primary rounded pb-2">
       <div className="humidity-header pt-2">
-        <h3>Humidity</h3>
+        <h4>Humidity</h4>
         <img
           src={humidityIcon}
           alt="Icon representing humidity"
@@ -22,6 +29,11 @@ export default function Humidity() {
           height="35px"
         />
       </div>
+      <div className="text-center">
+        <h2>{`${humidity}% `}</h2>
+        <p>{humidityDescription}</p>
+      </div>
+      <ProgressBar completed={`${humidity}`} animateOnRender="true" />
     </div>
   );
 }
