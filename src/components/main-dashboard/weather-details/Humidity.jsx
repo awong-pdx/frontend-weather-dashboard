d55dbc326aa26945033623bc78b0f6d5bfef9a7e
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-02 11:13:10 -0700

    Header for humidity component

--- src/components/main-dashboard/weather-details/Humidity.jsx
+++ src/components/main-dashboard/weather-details/Humidity.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useWeather } from '../../WeatherProvider';
+import humidityIcon from '../../../images/humidityIcon.png';
 
 export default function Humidity() {
+  const { currentWeather } = useWeather();
+  if (!currentWeather) return null;
+
+  const { humidity } = currentWeather.main;
+
+  // DELETE LATER
+  console.log(humidity);
+
   return (
     <div className="col-4 border border-primary rounded">
-      <h1>Humidity</h1>
+      <div className="humidity-header pt-2">
+        <h3>Humidity</h3>
+        <img
+          src={humidityIcon}
+          alt="Icon representing humidity"
+          width="35px"
+          height="35px"
+        />
+      </div>
     </div>
   );
 }
