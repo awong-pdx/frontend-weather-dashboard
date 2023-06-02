f8685ea3f00a7bf4f482290ad9eecab6186e2ef6
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-01 21:16:00 -0700

    Implement Hourly Temperature Graph (#31)
    * Minor change to svg and time displayed
    
    * Rename component
    
    * Get hours for x-values line graph
    
    * Display minimal version of working graph for hourly temperature
    
    * Structure and style change to line chart
    
    * Center sum components vertically
    
    * Finish hourly temperature graph component
    
    * Change CSS property value to flex-start instead of start
    
    * Fix naming and temperature typo

--- src/components/main-dashboard/weather-details/FeelsLikeTemp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FeelsLikeTemp() {
-  return (
-    <div className="col-8 border border-primary rounded">
-      <h1>Feels Like Temp</h1>
-    </div>
-  );
-}
