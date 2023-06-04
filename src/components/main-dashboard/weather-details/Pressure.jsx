bf064b4c403d661a20884692164d769d638964ca
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-04 12:24:11 -0700

    Add header style to pressure

--- src/components/main-dashboard/weather-details/Pressure.jsx
+++ src/components/main-dashboard/weather-details/Pressure.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useWeather } from '../../WeatherProvider';
 
 export default function Pressure() {
+  const { currentWeather } = useWeather();
+  if (!currentWeather) return null;
+
+  const { pressure } = currentWeather.main;
+
+  // REMOVE LATER!
+  console.log(pressure);
+
   return (
-    <div className="col-4 border border-primary rounded">
-      <h1>Pressure</h1>
+    <div className="weather-detail col-4 border border-primary rounded">
+      <div className="weather-detail-header pt-2">
+        <h4>Pressure</h4>
+      </div>
     </div>
   );
 }
