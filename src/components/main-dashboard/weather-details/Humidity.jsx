daf6ea9a7cd8d5172cdd9bce964fb24f4ba794f3
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-04 01:13:05 -0700

    Implement humidity component (#33)
    * Header for humidity component
    
    * Display humidity levels and graph
    
    * Style humidity component
    
    * Create font-family variable in sass
    
    * Change margin of weather detail portion
    
    * Fix progress bar animation prop
    
    * Change progress bar text color and add media query for humidty icon
    
    * Define colors and descriptions for humidity levels
    
    * Change humidity icon
    
    * Modify humidity icon and layout
    
    * Change humidity icon size

--- src/components/main-dashboard/weather-details/Humidity.jsx
+++ src/components/main-dashboard/weather-details/Humidity.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import ProgressBar from '@ramonak/react-progress-bar';
+import { useWeather } from '../../WeatherProvider';
+import humidityIcon from '../../../images/humidity.png';
 
 export default function Humidity() {
+  const { currentWeather } = useWeather();
+  if (!currentWeather) return null;
+
+  const red = '#dc3545';
+  const yellow = '#ffc107';
+  const poorLevels = 'Poor humidity levels';
+  const fairLevels = 'Fair humidity levels';
+
+  // default description and color (green)
+  let humidityDescription = 'Ideal humidity levels';
+  let humidityBarColor = '#28a745';
+
+  const { humidity } = currentWeather.main;
+  if (humidity >= 70 || humidity < 25) {
+    humidityDescription = poorLevels;
+    humidityBarColor = red;
+  } else if (
+    (humidity < 70 && humidity >= 60) ||
+    (humidity >= 25 && humidity < 30)
+  ) {
+    humidityDescription = fairLevels;
+    humidityBarColor = yellow;
+  }
+
   return (
-    <div className="col-4 border border-primary rounded">
-      <h1>Humidity</h1>
+    <div className="humidity col-4 border border-primary rounded pb-3">
+      <div className="humidity-header pt-2">
+        <h4>Humidity</h4>
+        <img
+          src={humidityIcon}
+          className="humidity-icon"
+          alt="Icon representing humidity"
+          width="27px"
+          height="27px"
+        />
+      </div>
+      <div className="text-center">
+        <h1>{`${humidity}% `}</h1>
+        <p>{humidityDescription}</p>
+      </div>
+      <ProgressBar
+        completed={`${humidity}`}
+        animateOnRender
+        bgColor={humidityBarColor}
+        labelColor="black"
+      />
     </div>
   );
 }
