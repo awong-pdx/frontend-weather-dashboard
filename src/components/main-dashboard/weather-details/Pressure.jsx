e035642b96dba16c94ad8e9a2750690a978b51b2
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-06-05 02:06:33 -0700

    Implement pressure component (#41)
    * Create wind component
    
    * Created general class for weather detail header sizing and icon
    
    * Add header style to pressure
    
    * Display default pressure visual for layout purposes
    
    * Align pressure guage
    
    * Change gauge text
    
    * Modify pressure gauge layout
    
    * Center pressure barometer

--- src/components/main-dashboard/weather-details/Pressure.jsx
+++ src/components/main-dashboard/weather-details/Pressure.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { useWeather } from '../../WeatherProvider';
+import 'react-circular-progressbar/dist/styles.css';
+import pressureIcon from '../../../images/barometer.png';
 
 export default function Pressure() {
+  const { currentWeather } = useWeather();
+  if (!currentWeather) return null;
+  const { pressure } = currentWeather.main;
+
   return (
-    <div className="col-4 border border-primary rounded">
-      <h1>Pressure</h1>
+    <div className="weather-detail col-4 border border-primary rounded">
+      <div className="weather-detail-header pt-2">
+        <h4>Pressure</h4>
+        <img
+          src={pressureIcon}
+          className="pressure-icon"
+          alt="Icon representing atmospheric pressure"
+          width="30px"
+          height="30px"
+        />
+      </div>
+      <div className="center-pressure-gauge">
+        <div className="pressure-gauge-container">
+          <CircularProgressbar
+            className="pressure-gauge"
+            circleRatio={0.75}
+            value={pressure}
+            minValue={980}
+            maxValue={1050}
+            text={`${pressure}hPa`}
+            strokeWidth={6}
+            styles={buildStyles({
+              rotation: 1 / 2 + 1 / 8,
+              strokeLinecap: 'butt',
+              trailColor: '#eee',
+              textColor: '#000',
+              textSize: '21px',
+            })}
+          />
+        </div>
+      </div>
     </div>
   );
 }
